a3385383782a7d49bc2b0f923c1781c129a9ca56
Phil Wang <lucidrains@gmail.com>
2021-03-02 08:35:32 -0800

    add performer pytorch

--- omninet_pytorch/omninet_pytorch.py
+++ omninet_pytorch/omninet_pytorch.py
@@ -4,22 +4,26 @@ import torch.nn.functional as F
 
 from einops import rearrange, repeat
 
+# use Performer, as it had the best reported numbers
+
+from performer_pytorch import Performer
+
 # helpers
 
 def exists(val):
-	return val is not None
+    return val is not None
 
 # classes
 
 class Omninet(nn.Module):
-	def __init__(
-		self,
-		*,
-		dim,
-		dim_head = 64,
-		heads = 8
-	):
-		super().__init__()
+    def __init__(
+        self,
+        *,
+        dim,
+        dim_head = 64,
+        heads = 8
+    ):
+        super().__init__()
 
-	def forward(self, x):
-		return x
+    def forward(self, x):
+        return x
